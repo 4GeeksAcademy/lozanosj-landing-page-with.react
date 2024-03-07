2700d6636e79d6f7219623d2855a288dbd88a389
Jhonny Lozano <110267545+lozanosj@users.noreply.github.com>
2024-03-07 21:11:09 +0000

    Proyecto terminado

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
 
 			<div class="col pt-2 pb-2 pe-2 mt-3">
 				<div class="card h-100">
-				<img src="/img/rigo-baby.jpg" class="card-img-top" alt="No entiendo" />
+				<img src="https://picsum.photos/536/354" class="card-img-top" alt="..." />
 						<div class="card-body">
 							<h5 class="card-title text-center">Card title</h5>
 							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus unde illum quisquam obcaecati labore doloremque quod laboriosam laborum, in iure mollitia quis assumenda ab recusandae vitae deserunt. Numquam, iusto iste!</p>
@@ -19,7 +19,7 @@ const Card = () => {
 
 			<div class="col p-2 mt-3">
 				<div class="card h-100">
-					<img src="..." class="card-img-top" alt="por qué"/>
+					<img src="https://picsum.photos/536/354" class="card-img-top" alt="por qué"/>
 						<div class="card-body">
 							<h5 class="card-title text-center">Card title</h5>
 							<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, placeat. Accusamus quasi ex dolor aliquam animi. Assumenda distinctio recusandae cum odit minima provident eaque, aut harum? Aliquam animi suscipit sapiente.</p>
@@ -32,7 +32,7 @@ const Card = () => {
 
 			<div class="col p-2 mt-3">
 				<div class="card h-100">
-					<img src="..." class="card-img-top" alt="no salen"/>
+					<img src="https://picsum.photos/536/354" class="card-img-top" alt="no salen"/>
 						<div class="card-body">
 							<h5 class="card-title text-center">Card title</h5>
 							<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel rerum nam vero, adipisci aliquam quasi fugiat. Esse, dolor velit adipisci magnam aut tempora nisi! Architecto aspernatur iure accusamus ipsa nisi?</p>
@@ -45,7 +45,7 @@ const Card = () => {
 
 			<div class="col ps-2 pt-2 pb-2 mt-3">
 				<div class="card h-100">
-					<img src="..." class="card-img-top" alt="las imágenes"/>
+					<img src="https://picsum.photos/536/354" class="card-img-top" alt="las imágenes"/>
 						<div class="card-body">
 							<h5 class="card-title text-center">Card title</h5>
 							<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad numquam corporis doloremque est, facere similique? Quia earum, aut non ducimus repellendus blanditiis quasi nisi! Perspiciatis impedit accusantium nobis libero sequi!</p>
